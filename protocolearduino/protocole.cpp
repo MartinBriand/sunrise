8b608cf9ca552792d9cfce6413a702f186fcb5dd
MartinB <martin.briand23@gmail.com>
2020-04-28 17:35:36 +0200

    All protocole implemeted i Arduino

--- protocolearduino/protocole.cpp
+++ protocolearduino/protocole.cpp
@@ -1,8 +1,9 @@
 #include "protocole.h"
+
 // for this see this page -> https://www.tutorialspoint.com/cplusplus/cpp_this_pointer.htm
 
-
-void sendUint32_t(uint32_t a) { //be careful, reading small digits first
+//general functions
+void send_uint32_t(uint32_t a, Protocole *p) { //be careful, reading small digits first
   uint32_t acopy = a;
   uint8_t* buf;
   for (int i = 0; i<4; i++) {
@@ -12,49 +13,126 @@ void sendUint32_t(uint32_t a) { //be careful, reading small digits first
   Serial.write(buf, 4);
 }
 
-uint32_t receiveUint32_t() {
+uint8_t receive_byte(Protocole *p) { //read one byte from stream
+  uint8_t a;
+  Chrono chrono;
+  chrono.restart();
+  while ( chrono.elapsed() < p->MAX_TIME_TO_RECEIVE_A_BYTE) {
+    if (Serial.available()){
+      a = Serial.read();
+      return a;
+    }
+  }
+  p->send_error();
+}
+
+uint32_t receive_uint32_t(Protocole *p) { //reading big digits first
   uint32_t a = 0;
   for(int i = 0; i<4; i++) {
     a*=256;
-    a+=Serial.read();
+    a+=receive_byte(p);
   }
   return a;
 }
 
+int32_t receive_int32_t(Protocole *p) {
+  int64_t a = receive_uint32_t(p);
+  int32_t b = a-2147483648;
+  return b;
+}
+
+void receive_vector_of_8_int32_t (int32_t vector[], Protocole *p) {
+  for (int i = 0; i<8; i++) {
+    vector[i] = receive_int32_t(p);
+  }
+}
+
+
+//protocole functions
 bool Protocole::is_initialized () {
-  return (this->data_initialized && this->speed_of_iter_initialized && this->pos_0_initialized); 
+  return (this->data_initialized && this->speed_of_iter_initialized && this->pos_0_initialized && this->memory_initialized); 
 }
 
 void Protocole::init() {
-  isStarted = false;
-  data_initialized = false;
-  speed_of_iter_initialized = false;
-  pos_0_initialized = false;
+  //initialize
+  this->is_started = false;
+  this->data_initialized = false;
+  this->speed_of_iter_initialized = false;
+  this->pos_0_initialized = false;
+  this->memory_initialized = false;
+  //ack
+  this->send_ack();
 }
 
-void Protocole::receive_speed() {
-	//ici on reçoit la vitesse
-
+void Protocole::receive_speed_of_iter() {
+  //receive
+  this->speed_of_iter = receive_uint32_t (this);
+  //ack
+  this->speed_of_iter_initialized = true;
+  send_uint32_t(this->speed_of_iter, this);
 }
 
 void Protocole::receive_pos_0() {
+  //receive
+  receive_vector_of_8_int32_t(this->pos_0, this);
+  //ack
+  this->pos_0_initialized = true;
+  this->send_ack();
+}
 
+void Protocole::send_memory() {
+  //send
+  send_uint32_t(this->max_number_of_instructions, this);
+  //check
+  uint32_t check = receive_uint32_t(this);
+  if (check == this->max_number_of_instructions) {
+    //ack
+    this->memory_initialized = true;
+    this->send_ack();
+  } else {
+    //error
+    this->send_error();
+  }
 }
 
 void Protocole::receive_data() {
+  //receive
+  this->data = new int32_t *[this->max_number_of_instructions];
+  for (int i = 0 ; i < this->max_number_of_instructions; i++) {
+    this->data[i] = new int32_t[8];
+  }
+  
+  for(int i = 0 ; i < this->max_number_of_instructions; i++) {
+    receive_vector_of_8_int32_t(this->data[i], this);
+  }
+  //ack
+  this->send_ack();
+}
 
+void Protocole::receive_error() {
+  this->init();
 }
 
 void Protocole::stop_motors() {
-
+  this->is_started = false;
+  this->send_ack();
 }
 
 void Protocole::start_motors() {
-
+  if (this->is_initialized()) {
+    this->is_started = true;
+    this->send_ack();
+  } else {
+    this->send_error();
+  }
 }
 
 void Protocole::send_error() {
+  Serial.write(CODESEND::ERRORARD);
+}
 
+void Protocole::send_ack() {
+  Serial.write(CODESEND::ACK);
 }
 
 void Protocole::one_step_motors() {
@@ -62,34 +140,4 @@ void Protocole::one_step_motors() {
   //Elle doit ensuite incrémenter le pointeur. si les données sont complètes, faire appelle à feed
 
 }
-
-
-
-
-
-// uint16_t inByte;
-//   uint8_t lengthOfVector = 3;
-//   uint16_t vector[lengthOfVector];
-//   uint8_t compteur = 0;
-//   uint16_t res = 0;
-  
-//   uint8_t sendUInt16_t[2];
-
-
-//   while (compteur < lengthOfVector) {
-//     if (Serial.available()>=2) {
-//       inByte = Serial.read()*256+Serial.read();
-//       vector[compteur] = inByte; // read the incoming data   
-//       compteur++;
-//     }
-//   }
-
-//   if (compteur == lengthOfVector) {
-//     for (int k = 0; k < lengthOfVector; k++) {
-//       res+=vector[k];
-//     }
-//     uint16_tToBuf(res, sendUInt16_t);
-//   }
-
-
   
--- protocolearduino/protocolearduino.ino
+++ protocolearduino/protocolearduino.ino
@@ -13,30 +13,36 @@ void loop() {
   if (Serial.available() == 1) {
     inByte = Serial.read();
     switch (inByte) {
-      case CODERECEIVE::initial :
+      case CODERECEIVE::INITIAL :
         p.init();
         break;
-      case CODERECEIVE::vitesse :
-        p.receive_speed();
+      case CODERECEIVE::SPEED :
+        p.receive_speed_of_iter();
         break;
-      case CODERECEIVE::pos_0 :
+      case CODERECEIVE::POS_0 :
         p.receive_pos_0();
         break;
-      case CODERECEIVE::data :
+      case CODERECEIVE::MEMORY :
+        p.send_memory();
+        break;
+      case CODERECEIVE::DATA :
         p.receive_data();
         break;
-      case CODERECEIVE::stop :
+      case CODERECEIVE::STOP :
         p.stop_motors();
         break;
-      case CODERECEIVE::start :
+      case CODERECEIVE::START :
         p.start_motors();
+        break;
+      case CODERECEIVE::ERRORPY :
+        p.receive_error();
         break;
       default :
         p.send_error();
     }
   } else if (Serial.available() > 1) {
     p.send_error();
-  } else if (p.isStarted) {
+  } else if (p.is_started) {
     p.one_step_motors();
   }
 }
